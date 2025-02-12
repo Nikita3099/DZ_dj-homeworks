0cf09bc32856323aa21608f061a9dc9bd5821d5f
Nikita3099 <n39335875@gmail.com>
2025-02-13 01:12:35 +0300

    DZ_Database-m2m

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Tag, Scope
 
-from .models import Article
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        main_count = sum(1 for form in self.forms if form.cleaned_data.get('is_main'))
+        if main_count != 1:
+            raise ValidationError('У каждой статьи должен быть один и только один основной тег.')
 
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 1
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,16 +1,38 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 
 class Article(models.Model):
-
-    title = models.CharField(max_length=256, verbose_name='Название')
-    text = models.TextField(verbose_name='Текст')
+    title = models.CharField(max_length=255)
+    text = models.TextField()
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(upload_to='articles/', blank=True, null=True)
+    tags = models.ManyToManyField('Tag', through='Scope')
 
     class Meta:
-        verbose_name = 'Статья'
-        verbose_name_plural = 'Статьи'
+        ordering = ['-id']
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="scopes")
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    is_main = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-is_main', 'tag__name']
+
+    def clean(self):
+        if self.is_main and Scope.objects.filter(article=self.article, is_main=True).exclude(pk=self.pk).exists():
+            raise ValidationError("У статьи может быть только один основной тег.")
+
+    def __str__(self):
+        return f"{self.article.title} - {self.tag.name}"
